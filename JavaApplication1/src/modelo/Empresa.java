346759bbfab3d2e2197f51fb9d2317bf74d6927d
Administrador <Administrador@10.10.4.8>
2019-09-19 16:42:01 -0300

    aaaaaaaaaaaaaaaaaaaaa

---
+++ JavaApplication1/src/modelo/Empresa.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Empresa {
+    private String nome_fantasia;
+    private String razao_social;
+    private Integer codigo;
+    private String abertura;
+    private String fundacao;
+
+    public String getAbertura() {
+        return abertura;
+    }
+
+    public void setAbertura(String abertura) {
+        this.abertura = abertura;
+    }
+
+    public String getFundacao() {
+        return fundacao;
+    }
+
+    public void setFundacao(String fundacao) {
+        this.fundacao = fundacao;
+    }
+
+    @Override
+    public String toString() {
+        return nome_fantasia;
+    }
+
+    public String getNome_fantasia() {
+        return nome_fantasia;
+    }
+
+    public void setNome_fantasia(String nome_fantasia) {
+        this.nome_fantasia = nome_fantasia;
+    }
+
+    public String getRazao_social() {
+        return razao_social;
+    }
+
+    public void setRazao_social(String razao_social) {
+        this.razao_social = razao_social;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+}
